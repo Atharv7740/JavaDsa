cdba1c042aca6c8fcc4da77813802be33a0848d5
Atharv7740 <atharv7740@gmail.com>
2024-03-13 16:46:19 +0530

    write a hello world program

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        String name;
+        System.out.print("Enter your Good Name:");
+        name=sc.nextLine();
+        System.out.println("Hello "+name);
+
+
+    }
+}
