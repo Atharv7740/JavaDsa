682d9b38155bd4e9f580f7d5fa975f6db9712a9c
Atharv7740 <atharv7740@gmail.com>
2024-03-13 17:44:04 +0530

    try2 package and class

---
+++ try2/Try2Class.java
@@ -0,0 +1,12 @@
+package try2;
+
+public class Try2Class {
+    public static void main(String[] args) {
+        hello();
+
+    }
+
+    public static void hello(){
+        System.out.println("Sample Code");
+    }
+}
