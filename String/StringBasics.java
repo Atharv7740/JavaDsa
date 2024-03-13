a26906dec657b23ffe29c14b8e73fb549fc315b4
Atharv7740 <atharv7740@gmail.com>
2024-03-13 17:53:46 +0530

    Strings basic

---
+++ String/StringBasics.java
@@ -0,0 +1,15 @@
+package String;
+
+public class StringBasics {
+    public static void main(String[] args) {
+        String n="Hello World";
+        System.out.println(n);
+        welcome();
+    }
+    public static void welcome(){
+        String name;
+        name="Atharv Tripathi";
+        System.out.println("Welcome Mr. " + name);
+    }
+
+}
