719bccc087965b6c002d015918be4c3c5fcc0a0f
Atharv7740 <atharv7740@gmail.com>
2024-03-15 21:09:40 +0530

    string update

--- String/StringBasics.java
+++ String/StringBasics.java
@@ -1,15 +1,25 @@
 package String;
 
 public class StringBasics {
+    static String  name="Ram Tripathi";
+    static String name2= new String("Mohit Tripathi");
     public static void main(String[] args) {
         String n="Hello World";
         System.out.println(n);
         welcome();
+        System.out.println(name);
+        System.out.println(name2);
+
     }
     public static void welcome(){
         String name;
         name="Atharv Tripathi";
         System.out.println("Welcome Mr. " + name);
     }
+
+    //There are two ways to declear a string ;
+//    using string litral
+
+
 
 }
